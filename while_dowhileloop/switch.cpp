bd0e91b8000bd52d89fc8d5e85be4d8dd72a5ae2
Azeem Rajput <muhammadaxeem24@gmail.com>
2025-08-01 02:33:22 +0500

    added initial code

---
+++ while_dowhileloop/switch.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+    cout<<"Enter a number=";
+    cin>>i;
+
+    switch(i)
+    {
+        case 1:
+        cout<<"Azeem";
+        break;
+
+        case 2:
+        cout<<"Abdulrehman";
+        break;
+
+        
+        default:
+        cout<<"sheikh";
+    }
+
+}
---
+++ while_dowhileloop/switchchar.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char i;
+    cout<<"Enter a number=";
+    cin>>i;
+
+    switch(i)
+    {
+        case 'a':
+        cout<<"Azeem";
+        break;
+
+        case 'b':
+        cout<<"Abdulrehman";
+        break;
+
+        
+        default:
+        cout<<"sheikh";
+    }
+
+}
