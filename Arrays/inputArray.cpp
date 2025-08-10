4dc4934c620a52e35c13e92da2010fff48768f91
Azeem Rajput <muhammadaxeem24@gmail.com>
2025-08-10 07:31:05 +0500

    added initial code

---
+++ Arrays/inputArray.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[5];
+    cout<<"Enter the array=";
+    for(int i=0;i<=4;i++)\
+    {
+        cin>>arr[i];
+    }
+    return 0;
+}
