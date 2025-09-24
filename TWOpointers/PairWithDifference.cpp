465df09cdafdfab31c6a5433fec6a1ab814fb6ed
Azeem Rajput <muhammadaxeem24@gmail.com>
2025-09-25 00:01:50 +0500

    added initial code

---
+++ TWOpointers/PairWithDifference.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+int pairdifference(int arr[],int n,int target)
+{
+    int start=0,end=1;
+    while(end<n)
+    {
+        if(arr[start]+arr[end]==target)
+        {
+            return 1;
+        }
+        else if(arr[start]+arr[end]<target)
+        {
+            end++;
+        }
+        else
+        {
+            start++;
+        }
+        return 0;
+    }
+}
+int main()
+{
+    int arr[100];
+    int n;
+    cout<<"Enter the size of array=";
+    cin>>n;
+    int t;
+    cout<<"Enter the reqiured number=";
+    cin>>t;
+    cout<<"Enter an array=";
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<pairdifference;
+}
