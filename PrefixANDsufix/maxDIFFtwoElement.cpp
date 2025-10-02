aae8412b007c841404479838fd35f3dc73b3889c
Azeem Rajput <muhammadaxeem24@gmail.com>
2025-10-03 02:47:27 +0500

    added initial code

---
+++ PrefixANDsufix/maxDIFFtwoElement.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<limits.h>
+using namespace std;
+int MaxBT(int n,int arr[])
+{
+    int ans=INT_MIN;
+    int suffix=arr[n-1];
+    for(int i=n-2;i>=0;i--)
+    {
+        ans=max(ans,arr[i]-suffix);
+        suffix=max(suffix,arr[i]);
+    }
+    return ans;
+}
+int main()
+{
+    int arr[1000];
+    int n;
+    cout<<"Enter a size eof array=";
+    cin>>n;
+
+    cout<<"Enter an array=";
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<MaxBT(n,arr);
+
+}
