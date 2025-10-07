84be56aad39dd902d900a68c057173228c72a6a9
Azeem Rajput <muhammadaxeem24@gmail.com>
2025-10-08 01:20:09 +0500

    added initial code

--- LeetcodeProblems/TrappingRainWater1.cpp
+++ LeetcodeProblems/TrappingRainWater1.cpp
@@ -3,13 +3,28 @@
 using namespace std;
 int TrappingRain(int n,int arr[])
 {
-    vector<int>arr;
+    int miniheight[1000];
     int leftmax[n],rightmax[n];
     leftmax[0]=0;
     for(int i=1;i<n;i++)
     {
-        leftmax[i]=max(leftmax[i-1],rightmax[i-1]);
+        leftmax[i]=max(leftmax[i-1],arr[i-1]);
     }
+    rightmax[n-1]=0;
+    for(int i=n-2;i>0;i--)
+    {
+        rightmax[i]=max(rightmax[i+1],arr[i+1]);
+    }
+    int water=0;
+    for(int i=0;i<n;i++)
+    {
+        miniheight[i]=min(leftmax[i],rightmax[i]);
+        if(miniheight[i]-arr[i]>=0)
+        {
+            water+=miniheight[i]-arr[i];
+        }
+    }
+    return water;
 }
 int main()
 {
