ab725d73a9cc57ffb2d80f226dfb8f4141bfc2db
Azeem Rajput <muhammadaxeem24@gmail.com>
2025-10-07 23:41:05 +0500

    added initial code

--- LeetcodeProblems/TrappingRainWater1.cpp
+++ LeetcodeProblems/TrappingRainWater1.cpp
@@ -1,6 +1,20 @@
 #include<iostream>
 using namespace std;
-int main()
+int TrappingRain(int n,int arr[])
 {
     
+}
+int main()
+{
+    int arr[1000];
+    int n;
+    cout<<"Enter size of array=";
+    cin>>n;
+
+    cout<<"Enter array=";
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+   cout<<TrappingRain(n,arr);
 }
