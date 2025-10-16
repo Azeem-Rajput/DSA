71eb2a6d9b9578ed1a4cec285f71766c11e50398
Azeem Rajput <muhammadaxeem24@gmail.com>
2025-10-16 22:55:42 +0500

    added initial code

--- oops/construcIMPLE.cpp
+++ oops/construcIMPLE.cpp
@@ -8,12 +8,29 @@ class customer
     int balence;
 
     public:
+    //Default Constructor    
     customer()
     {
-        cout<<"this is DEFAULT CONSTRUCTOR";
+        name="AZeem";
+        accno=200;
+        balence=10000;
+    }
+    //Parameterized Constructor
+    customer(string a,int b,int c)
+    {
+        name=a;
+        accno=b;
+        balence=c;
+    }
+    void show()
+    {
+        cout<<name<<" "<<accno<<" "<<balence<<endl;
     }
 };
 int main()
 {
     customer A1;
+    customer A2("Rajut",55,10000);
+    A1.show();
+    A2.show();
 }
