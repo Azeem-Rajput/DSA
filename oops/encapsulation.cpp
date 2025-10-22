cab40b8869a24c0971ad8c0813699fb52c205391
Azeem Rajput <muhammadaxeem24@gmail.com>
2025-10-23 02:20:41 +0500

    added initial code

--- oops/encapsulation.cpp
+++ oops/encapsulation.cpp
@@ -6,18 +6,21 @@ class customer
 {
     string name;
     int accno,bal;
+    int age;
     static int totalcustomer;
     static int totalbalence;
 
     public:
+    
     // for directly access
     //static int totalcustomer;
 
-    customer(string x,int y,int z)
+    customer(string x,int y,int z,int ag)
     {
         name=x;
         accno=y;
         bal=z;
+        age=ag;
         totalcustomer++;
         totalbalence+=z;
     }
@@ -26,6 +29,18 @@ class customer
     {
         cout<<"Total customers="<<totalcustomer<<endl;
         cout<<"Total balence="<<totalbalence;
+    }
+
+    void updateage(int year)
+    {
+        if(year>0 && year<100)
+        {
+            age=year;
+        }
+        else
+        {
+            cout<<"enter valid age"<<endl;
+        }
     }
 
     void deposit(int amount)
@@ -52,7 +67,7 @@ class customer
 
     void display()
     {
-        cout<<name<<" "<<accno<<" "<<bal<<" "<<totalcustomer<<endl;
+        cout<<name<<" "<<accno<<" "<<bal<<" "<<totalcustomer<<" "<<age<<endl;
     }
 
     void tot()
@@ -67,13 +82,14 @@ int customer::totalbalence=0;
 
 int main()
 {
-    customer A1("AZEEM",3910,1000);
-    customer A2("RAJPUT",3210,1200);
-    customer A3("HAMZA",3390,1000);
+    customer A1("AZEEM",3910,1000,2);
+    customer A2("RAJPUT",3210,1200,10);
+    customer A3("HAMZA",3390,1000,-10);
 
     // Directly access by using static function using below
     A1.deposit(-10);
-    customer::accessfun();
+    A1.updateage(110);
+    A1.display();
     
 
 }
---
+++ oops/tempCodeRunnerFile.cpp
@@ -0,0 +1,1 @@
+A1.updateage(24);
