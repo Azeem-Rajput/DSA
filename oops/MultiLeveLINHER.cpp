84300c4921976ee9364c61da5acdfeecd1c6ebee
Azeem Rajput <muhammadaxeem24@gmail.com>
2025-10-29 01:49:31 +0500

    added initial code

--- oops/MultiLeveLINHER.cpp
+++ oops/MultiLeveLINHER.cpp
@@ -4,17 +4,50 @@ using namespace std;
 class person
 {
     protected:
-    int name,age;
+    string name;
+    int age;
+
+    public:
+    void display()
+    {
+        cout<<"person class works with name & age="<<name<<" "<<age<<endl;
+    }
 };
 
 class employee:public person
 {
     protected:
     int salary;
+
+    public:
+    void display()
+    {
+    cout<<"employe class works with salary="<<salary<<endl;
+    }
 };
 class manager:public employee
 {
     protected:
     string dep;
-    int no_emp;
+    
+    public:
+    manager(string name,int age,int salary, string dep)
+    {
+        this->name=name;
+        this->age=age;
+        this->salary=salary;
+        this->dep=dep;
+    }
+    
+    void print_manage()
+    {
+        cout<<name<<" "<<age<<" "<<salary<<" "<<dep<<endl;
+    }
 };
+
+int main()
+{
+    manager A1("Azeem",12,110,"socialmedia");
+    A1.print_manage();
+
+}
