a1ade9a13fdb2c86caf7ed792be3b799d97f4577
Azeem Rajput <muhammadaxeem24@gmail.com>
2025-10-28 23:59:25 +0500

    added initial code

---
+++ oops/MultiLeveLINHER.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+class person
+{
+    protected:
+    int name,age;
+};
+
+class employee
+{
+    protected:
+    int salary;
+};
+class manager
+{
+    protected:
+    string dep;
+    int no_emp;
+};
