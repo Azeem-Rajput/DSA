fdcb9bfc2f0b270cc960f1940e023351e5c20463
Azeem Rajput <muhammadaxeem24@gmail.com>
2025-11-08 00:05:05 +0500

    added initial code

---
+++ oops/Exception2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<exception>
+using namespace std;
+
+int main()
+{
+    try{
+    int *p=new int[1000000000];
+    cout<<"Memory allocated completed"<<endl;
+    delete []p;
+    }
+    catch(const exception &e)
+    {
+        cout<<"Exception Occur "<<e.what()<<endl;
+    }
+} 
