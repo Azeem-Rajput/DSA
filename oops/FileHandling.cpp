4152508bf1c3691bf20def703048e6cfebf041a2
Azeem Rajput <muhammadaxeem24@gmail.com>
2025-11-14 01:45:25 +0500

    added initial code

---
+++ oops/FileHandling.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main()
+{
+    // File ko open krna
+    ofstream fout;
+    fout.open("zoom.txt");// agar ye file available na hui tou isko create kr dy ga
+    // Write krna
+    fout<<"Hello Azeem";
+
+    fout.close();
+}
