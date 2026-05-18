aa94742c4809b3b5d5c2176d513c0c3cb966a0e6
Azeem Rajput <muhammadaxeem24@gmail.com>
2026-05-19 01:39:39 +0500

    added recursion code

---
+++ DSA/recursion.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+void fn3(int n)
+{
+    if(n==0)
+    {
+        cout<<"Happy Birthday"<<endl;
+        return;
+    }
+    cout<<n<<" days left for birthday"<<endl;
+    fn3(n-1);
+}
+int main()
+{
+    // int n=5;
+    // for(int i=n;i>0;i--)
+    // {
+    //     cout<<i<<" days left for birthday"<<endl;
+    // }
+    // cout<<"Happy Birthday";
+
+
+    fn3(5);
+}
