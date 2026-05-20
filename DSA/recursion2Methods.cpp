ef26143fa8131351d0371a0d3ad395c6d7f4c007
Azeem Rajput <muhammadaxeem24@gmail.com>
2026-05-21 03:22:44 +0500

    added recursion code

---
+++ DSA/recursion2Methods.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+// void p(int num,int n)
+// {
+//     // Print even numbers 1 to n (two arguments)
+//     if(num>n)
+//     {
+//         return;
+//     }
+//     cout<<num;
+//     p(num+2,n);
+// }
+// int main()
+// {
+//     int n;
+//     cin>>n;
+//     if(n%2==1)
+//     n--;
+//     p(2,n);
+// }
+
+
+
+
+
+void print(int N)
+{
+    //Print even numbers 1 to n(one argument)
+    if(N==2)
+    {
+        cout<<2<<" ";
+        return;
+    }
+    print(N-2);
+    cout<<N<<" ";
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    if(n%2==1)
+    n--;
+    print(n);   
+}
