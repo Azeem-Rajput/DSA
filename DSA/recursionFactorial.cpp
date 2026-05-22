303fc6f8879d8febd2902cd4619558b420e1ca01
Azeem Rajput <muhammadaxeem24@gmail.com>
2026-05-22 23:22:45 +0500

    added recursion code

---
+++ DSA/recursionFactorial.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int fact(int N)
+{
+    if(N==1 || N==0)
+    {
+        return 1;
+    }
+    return N*fact(N-1);
+}
+int main()
+{
+    int n=0;
+    cout<<fact(n);
+}
