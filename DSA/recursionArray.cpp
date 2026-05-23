9563dbc131cb76d1ff9c630f51502f1433f79fa0
Azeem Rajput <muhammadaxeem24@gmail.com>
2026-05-23 21:00:04 +0500

    added recursion code

---
+++ DSA/recursionArray.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+void p(int arr[],int index,int n)
+{
+    //break case
+    if (index==n)
+    {
+        return;
+    }
+    p(arr,index+1,n);
+    cout<<arr[index]<<" ";
+    
+}
+int main()
+{
+    int arr[]={3,7,6,2,8};
+    p(arr,0,5);
+}
