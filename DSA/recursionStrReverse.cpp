622e5f3334d4e8616aafca89afd8a390a9729db9
Azeem Rajput <muhammadaxeem24@gmail.com>
2026-06-06 15:04:58 +0500

    added recursion code

---
+++ DSA/recursionStrReverse.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+void reverse(string &str,int start,int end)
+{
+
+    //base case
+    if(start>=end)
+    {
+        return;
+    }
+
+    char c=str[start];
+    str[start]=str[end];
+    str[end]=c;
+    reverse(str,start+1,end-1);
+}
+int main()
+{
+    string str="azeem";
+    reverse(str,0,4);
+    cout<<str<<endl;
+
+}
