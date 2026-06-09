96447e8ec0b4d7e767c4bb05084fb863e36e4f42
Azeem Rajput <muhammadaxeem24@gmail.com>
2026-06-10 03:00:41 +0500

    added recursion code

--- DSA/recursionSubsetSum.cpp
+++ DSA/recursionSubsetSum.cpp
@@ -1,30 +1,37 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-void subsequence(int arr[],int index,int n,int sum,vector<int>&ans,vector<int>&temp)
+void subsequence(int arr[],int index,int n,int sum,vector<pair<int, vector<int>>> &ans,vector<int>&temp)
 {
     // base case
     if(index==n)
     {
-        ans.push_back(sum,temp);  
+        ans.push_back({sum,temp});  
         return;
     }
 
    //no 
    subsequence(arr,index+1,n,sum,ans,temp);
    // yes
-   subsequence(arr,index+1,n,sum+arr[index],ans,temp);
    temp.push_back(arr[index]);
+   subsequence(arr,index+1,n,sum+arr[index],ans,temp);
+   temp.pop_back();
+   
 }
 int main() 
 {
     int arr[]={3,4,5};
-    vector<int>ans;
+    vector<pair<int, vector<int>>> ans;
     vector<int>temp;
     subsequence(arr,0,3,0,ans,temp);
     for(int i=0;i<ans.size();i++)
     {
-        cout<<ans[i]<<endl;
+        cout<<ans[i].first<<" ";
+        for(int j=0;j<ans[i].second.size();j++)
+        {
+            cout<<ans[i].second[j]<<" ";
+        }
+        cout<<endl;
     }
 
 }
