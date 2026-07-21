1bb00b98dfdca8a185f67d24fa66fc2a890675f9
Azeem Rajput <muhammadaxeem24@gmail.com>
2026-07-22 04:46:17 +0500

    added stack code

---
+++ STACK/stackone.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+using namespace std;
+class stack
+{
+    int *arr;
+    int size;
+    int top;
+    public:
+    //constructor
+    stack(int s)
+    {
+        size=s;
+        top=-1;
+        arr=new int[s];
+    }
+    //push
+    //pop
+    //peek
+    //iSsize
+    //iSempty
+    void push(int value)
+    {
+        if(top==size-1)
+        {
+           cout<<"stack overflow"<<endl;
+        }
+        else
+        {
+             top++;
+            arr[top]=value;
+            cout<<value<<" pushed into stack"<<endl;
+            
+        }
+    }
+    void pop()
+    {
+        if(top==-1)
+        {
+            cout<<"stack underflow"<<endl;
+        }
+        else
+        {
+            
+            cout<<arr[top]<<" popped from stack"<<endl;
+            top--;
+        }
+    }
+    void peek()
+    {
+        if(top==-1)
+        {
+            cout<<"stack underflow"<<endl;
+            cout<<top<<endl;
+        }
+        else
+        {
+        cout<<arr[top]<<" is on peak of stack"<<endl;
+        }
+    }
+    bool isempty()
+    {
+        return top==-1;
+    }
+    void iSsize()
+    {
+        cout<<top+1<<endl;
+    }
+
+};
+int main()
+{
+    stack s(5);
+    s.peek();
+}
