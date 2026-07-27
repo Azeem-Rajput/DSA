99ffa5383fd2baa48ff3820e01fab1ffa792434a
Azeem Rajput <muhammadaxeem24@gmail.com>
2026-07-28 04:47:53 +0500

    added sorting code

---
+++ QuickSort/quick.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int partition(int arr[],int start,int end)
+{
+    int pos=start;
+    for(int i=start;i<=end;i++)
+    {
+        if(arr[i]<=arr[end])
+        {
+            swap(arr[pos],arr[i]);
+            pos++;
+        }
+    }
+    return pos-1;
+}
+void quickSort(int arr[],int start,int end)
+{
+    if(start>=end)
+    {
+        return;
+    }
+    int pivot=partition(arr,start,end);
+    quickSort(arr,start,pivot-1);
+    quickSort(arr,pivot,end);
+}
+int main()
+{
+    int arr[]={6,2,5,1,4,2,3};
+    quickSort(arr,0,6);
+    for(int i=0;i<7;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
