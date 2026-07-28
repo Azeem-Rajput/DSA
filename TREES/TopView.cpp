d22a099acd78fc03331528461b6b7819a9ad1a84
Azeem Rajput <muhammadaxeem24@gmail.com>
2026-07-29 01:40:28 +0500

    added tree  code

---
+++ TREES/TopView.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<queue>
+#include<map>
+using namespace std;
+class Node
+{
+    public:
+    int data;
+    Node*left,*right;
+    Node(int value)
+    {
+        data=value;
+        left=NULL;
+        right=NULL;
+    }    
+};
+void topView(Node* root)
+{
+    queue<pair<Node*, int>>q;
+    map<int ,int>m;
+    q.push({root,0});
+
+    while(q.size()>0)
+    {
+        Node*curr=q.front().first;
+        int currHD=q.front().second;
+        q.pop();
+        if(m.find(currHD)==m.end())
+        {
+            m[currHD]=curr->data;
+        }
+        if(curr->left!=NULL)
+        {
+            q.push({curr->left,currHD-1});
+        }
+        if(curr->right!=NULL)
+        {
+            q.push({curr->right,currHD+1});
+        }
+    }
+
+    for(auto it : m)
+    {
+        cout<<it.second<<" ";
+    }
+    cout<<endl;
+
+}
